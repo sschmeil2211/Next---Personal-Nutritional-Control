447ce929e2c1ffaa34e6db3f7d14f33e6d9b5b65
sschmeil22 <sebaschmeil123@gmail.com>
2024-01-23 21:47:15 -0300

    simple web admin

---
+++ src/app/firebase_service.tsx
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyATzUpFa2MCcC5CR4-DVTzvtWTeW0bvJwg",
+  authDomain: "personal-nutrition-control.firebaseapp.com",
+  databaseURL: "https://personal-nutrition-control-default-rtdb.firebaseio.com",
+  projectId: "personal-nutrition-control",
+  storageBucket: "personal-nutrition-control.appspot.com",
+  messagingSenderId: "797970486297",
+  appId: "1:797970486297:web:fc2d6364f3a0c065298688",
+  measurementId: "G-CRY2HGGG9X"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig); 
+export const db = getFirestore(app);
+const analytics = getAnalytics(app);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,110 @@
-import Image from "next/image";
+"use client"
+
+import { ChangeEvent, useState } from "react";
 import styles from "./page.module.css";
+import { collection, addDoc, setDoc, doc } from 'firebase/firestore';
+import { v4 } from 'uuid'; // Importa uuid
+import { db } from "./firebase_service";// Importa tu instancia de Firestore desde donde esté configurada
+ 
+enum FoodType {
+  Dairy = "FoodType.dairy",
+  Drink = "FoodType.drink",
+  Fruit = "FoodType.fruit",
+  Grain = "FoodType.grain",
+  Legume = "FoodType.legume",
+  Meat = "FoodType.meat",
+  Nut = "FoodType.nut",
+  Processed = "FoodType.processed",
+  Sweet = "FoodType.sweet",
+  Vegetable = "FoodType.vegetable",
+  Other = "FoodType.other",
+}
+
+interface Inputs {
+  id: string;
+  addedBy: string;
+  name: string;
+  foodType: FoodType;
+  calories: number;
+  proteins: number;
+  carbs: number;
+  fats: number;
+}
 
 export default function Home() {
+  const initialInputs: Inputs = {
+    id: "",
+    name: "",
+    foodType: FoodType.Other,
+    calories: 0,
+    proteins: 0,
+    carbs: 0,
+    fats: 0,
+    addedBy: "app",
+  };
+  
+  const [inputs, setInputs] = useState<Inputs>(initialInputs);
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { id, value } = e.target;
+    const sanitizedValue = id === 'calories' || id === 'proteins' || id === 'carbs' || id === 'fats'
+    ? value.replace(',', '.')
+    : value;
+    setInputs((prevInputs) => ({ ...prevInputs, [id]: id === 'name' ? sanitizedValue : id === 'foodType' ? sanitizedValue as FoodType : parseFloat(sanitizedValue) }));
+  };
+
+  const handleSaveClick = async () => {
+    try { 
+      const uniqueId = v4(); // Genera un id único con uuid
+      const newInputs = { ...inputs, id: uniqueId };
+      await setDoc(doc(db, 'foods', uniqueId),  newInputs);  
+      setInputs(initialInputs);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main className={styles.container}>
+      <div className={styles.inputContainer}>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" onChange={handleInputChange} />
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className={styles.inputContainer}>
+        <label htmlFor="foodType">Food Type</label>
+        <select id="foodType" onChange={handleInputChange} value={inputs.foodType}>
+          {Object.values(FoodType).map((type) => (
+            <option key={type} value={type}>
+              {type}
+            </option>
+          ))}
+        </select>
       </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className={styles.inputContainer}>
+        <label htmlFor="calories">Calories</label>
+        <input type="number" id="calories" onChange={handleInputChange} />
       </div>
+
+      <div className={styles.inputContainer}>
+        <label htmlFor="proteins">Proteins</label>
+        <input type="number" id="proteins" onChange={handleInputChange} />
+      </div>
+
+      <div className={styles.inputContainer}>
+        <label htmlFor="carbs">Carbs</label>
+        <input type="number" id="carbs" onChange={handleInputChange} />
+      </div>
+
+      <div className={styles.inputContainer}>
+        <label htmlFor="fats">Fats</label>
+        <input type="number" id="fats" onChange={handleInputChange} />
+      </div>
+
+      <button className={styles.saveButton} onClick={handleSaveClick}>
+        Guardar
+      </button>
     </main>
   );
 }
