53ff000d03f744b7780e70698982904e90eb1b55
sschmeil22 <sebaschmeil123@gmail.com>
2024-01-24 17:54:36 -0300

    food list and filters

---
+++ src/models/food.tsx
@@ -0,0 +1,25 @@
+export interface Food {
+    id: string;
+    addedBy: string;
+    name: string;
+    foodType: FoodType;
+    calories: number;
+    proteins: number;
+    carbs: number;
+    fats: number;
+    [key: string]: string | number; // Add an index signature
+}
+
+export enum FoodType {
+    Dairy = "FoodType.dairy",
+    Drink = "FoodType.drink",
+    Fruit = "FoodType.fruit",
+    Grain = "FoodType.grain",
+    Legume = "FoodType.legume",
+    Meat = "FoodType.meat",
+    Nut = "FoodType.nut",
+    Processed = "FoodType.processed",
+    Sweet = "FoodType.sweet",
+    Vegetable = "FoodType.vegetable",
+    Other = "FoodType.other",
+}
